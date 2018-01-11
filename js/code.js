6f9d973c91932aca29684236e2acef4c09125ac5
Andrew Omelchenko <andrewom43@gmail.com>
2018-01-11 09:33:43 +0200

    Go away from objects

--- js/code.js
+++ js/code.js
@@ -1,41 +1,19 @@
 var key = "c7dc2200f66725961863fb901fd1e553";
-var apiLink = "https://api.openweathermap.org/data/2.5/weather?";
+var apiLink = "http://api.openweathermap.org/data/2.5/weather?";
+var iconLink = "http://openweathermap.org/img/w/";
+var currentWeatherObject = null;
+var currentScreen = null;
+
 
 
 // Weather class
 var Weather = function() {
-	this.weatherState = "sn";
+	this.weatherState = "10d";
 	this.temperature = 0;
 	this.location = "London";
 	this.humidity = 0;
 	this.velocity = 0;
 	this.direction = "N";
-};
-// public methods
-Weather.prototype.get = function(loc) {
-	this._updateData(loc);
-};
-// private methods
-Weather.prototype._updateData = function(loc) {
-	var url = apiLink + "q=" + loc + "&APPID=" + key;
-	this._sendRequest(url);
-};
-Weather.prototype._sendRequest = function(url) {
-	var xmlhttp = new XMLHttpRequest();
-	xmlhttp.open("GET", url, true);
-	xmlhttp.onreadystatechange = function() {
-		if (xmlhttp.readystate === 4 && xmlhttp.status === 200) {
-			console.log("Inside");
-			var data = JSON.parse(xmlhttp.responseText);
-			this.weatherState = data.weather[0].id;
-			this.temperature = data.main.temp;
-			this.location = data.name;
-			this.humidity = data.main.humidity;
-			this.velocity = data.wind.speed;
-			this.direction = data.wind.deg;
-		}
-	};
-	xmlhttp.send();
 };
 
 // Screen class
@@ -53,40 +31,62 @@ var Screen = function(doc) {
 	this.velocityUnitsId = doc.getElementById("velocity-units");
 	this.directionId = doc.getElementById("direction");
 };
-Screen.prototype.update = function(weather) {
-	this.temperatureId.innerHTML = weather.temperature;
-	this.temperatureUnitsId.innerHTML = this.temperatureUnits;
-	this.locationId.innerHTML = weather.location;
-//	this.iconId.src = baseUrl + imagePath + weather.weatherState + ".svg";
-	this.humidityId.innerHTML = weather.humidity;
-	this.velocityId.innerHTML = weather.velocity;
-	this.velocityUnitsId.innerHTML = this.velocityUnits;
-	this.directionId.innerHTML = weather.direction;
-};
-Screen.prototype.switchUnits = function(units) {
-	if (this.currentUnits == units) return;
-	if (units == "si-base-units") {
-		this.currentUnits = "si-base-units";
-		this.temperatureUnits = " C";
-		this.velocityUnits = " m/s";
-	} else {
-		this.currentUnits = "us-customary-units";
-		this.temperatureUnits = " F";
-		this.velocityUnits = " mph";
-	}
-	// TODO: update screen
-	this.temperatureUnitsId.innerHTML = this.temperatureUnits;
-	this.velocityUnitsId.innerHTML = this.velocityUnits;
-};
 
 window.onload = function() {
-	var screen = new Screen(document);
-	var weather = new Weather();
-	weather.get("Kyiv");
-	screen.update(weather);
+	currentScreen = new Screen(document);
+	currentWeatherObject = new Weather();
+	getWeather("Kyiv");
+	updateScreen(currentScreen, currentWeatherObject);
 
 	// add event listener to Reset button
 	document.getElementById("base-units").addEventListener("click", function(event) {
-		screen.switchUnits(event.target.value);
+		switchUnits(currentScreen, event.target.value);
 	});
 }
+
+// utility functions
+function getWeather(loc) {
+	var url = apiLink + "q=" + loc + "&units=metric&APPID=" + key;
+	var xmlhttp = new XMLHttpRequest();
+	xmlhttp.open("GET", url, true);
+	xmlhttp.onreadystatechange = function() {
+		if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+			var data = JSON.parse(xmlhttp.responseText);
+			// console.log(data);
+			var weatherObject = new Weather();
+			weatherObject.weatherState = data.weather[0].icon;
+			weatherObject.temperature = data.main.temp;
+			weatherObject.location = data.name;
+			weatherObject.humidity = data.main.humidity;
+			weatherObject.velocity = data.wind.speed;
+			weatherObject.direction = data.wind.deg;
+			updateScreen(currentScreen, weatherObject);
+		}
+	};
+	xmlhttp.send();
+};
+function updateScreen(screen, weather) {
+	screen.temperatureId.innerHTML = weather.temperature;
+	screen.temperatureUnitsId.innerHTML = screen.temperatureUnits;
+	screen.locationId.innerHTML = weather.location;
+	screen.iconId.src = iconLink + weather.weatherState + ".png";
+	screen.humidityId.innerHTML = weather.humidity;
+	screen.velocityId.innerHTML = weather.velocity;
+	screen.velocityUnitsId.innerHTML = screen.velocityUnits;
+	screen.directionId.innerHTML = weather.direction;
+};
+function switchUnits(screen, units) {
+	if (screen.currentUnits == units) return;
+	if (units == "si-base-units") {
+		screen.currentUnits = "si-base-units";
+		screen.temperatureUnits = " C";
+		screen.velocityUnits = " m/s";
+	} else {
+		screen.currentUnits = "us-customary-units";
+		screen.temperatureUnits = " F";
+		screen.velocityUnits = " mph";
+	}
+	// TODO: update screen
+	screen.temperatureUnitsId.innerHTML = screen.temperatureUnits;
+	screen.velocityUnitsId.innerHTML = screen.velocityUnits;
+};
