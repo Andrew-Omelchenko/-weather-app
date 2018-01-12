a3533e32c43af43af059216ef0879084c4b05084
Andrew Omelchenko <andrewom43@gmail.com>
2018-01-12 08:45:46 +0200

    Add location search

--- js/code.js
+++ js/code.js
@@ -46,7 +46,15 @@ window.onload = function() {
 	getWeather("Kyiv");
 	updateScreen(currentScreen, currentWeatherObject);
 
-	// add event listener to Reset button
+	// add event listener to Go button
+	document.getElementById("go-button").addEventListener("click", function(event) {
+		var loc = document.getElementById("loc-field").value;
+		console.log(loc);
+		if (loc == "") return;
+		getWeather(loc);
+	});
+
+	// add event listener to select element
 	document.getElementById("base-units").addEventListener("click", function(event) {
 		switchUnits(currentScreen, event.target.value);
 	});
