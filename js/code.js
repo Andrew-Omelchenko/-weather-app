46243046be4d8fd470545eda6b2a5baed7b41c02
Andrew Omelchenko <andrewom43@gmail.com>
2018-01-12 08:22:21 +0200

    Fix temperature conversions

--- js/code.js
+++ js/code.js
@@ -101,12 +101,16 @@ function switchUnits(screen, units) {
 	if (units == "si-base-units") {
 		screen.currentUnits = "si-base-units";
 		screen.temperatureId.innerHTML = toCelsius(screen.temperatureId.innerHTML);
+		screen.tempMinId.innerHTML = toCelsius(screen.tempMinId.innerHTML);
+		screen.tempMaxId.innerHTML = toCelsius(screen.tempMaxId.innerHTML);
 		screen.temperatureUnits = "C";
 		screen.velocityId.innerHTML = toMs(screen.velocityId.innerHTML);
 		screen.velocityUnits = "m/s";
 	} else {
 		screen.currentUnits = "us-customary-units";
 		screen.temperatureId.innerHTML = toFahrenheit(screen.temperatureId.innerHTML);
+		screen.tempMinId.innerHTML = toFahrenheit(screen.tempMinId.innerHTML);
+		screen.tempMaxId.innerHTML = toFahrenheit(screen.tempMaxId.innerHTML);
 		screen.temperatureUnits = "F";
 		screen.velocityId.innerHTML = toMph(screen.velocityId.innerHTML);
 		screen.velocityUnits = "mph";
