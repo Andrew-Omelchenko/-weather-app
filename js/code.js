06c0f52dfaed829f711679e620f9c8432a15bcc5
Andrew Omelchenko <andrewom43@gmail.com>
2018-01-12 08:18:20 +0200

    Add temperature fields, convert directions

--- js/code.js
+++ js/code.js
@@ -1,6 +1,6 @@
-var key = "c7dc2200f66725961863fb901fd1e553";
-var apiLink = "https://api.openweathermap.org/data/2.5/weather?";
-var iconLink = "http://openweathermap.org/img/w/";
+const key = "c7dc2200f66725961863fb901fd1e553";
+const apiLink = "https://api.openweathermap.org/data/2.5/weather?";
+const iconLink = "http://openweathermap.org/img/w/";
 var currentWeatherObject = null;
 var currentScreen = null;
 
@@ -10,6 +10,8 @@ var currentScreen = null;
 var Weather = function() {
 	this.weatherState = "10d";
 	this.temperature = 0;
+	this.tempMin = 0;
+	this.tempMax = 0;
 	this.location = "London";
 	this.description = "none";
 	this.humidity = 0;
@@ -21,10 +23,14 @@ var Weather = function() {
 var Screen = function(doc) {
 	this.doc = doc;
 	this.currentUnits = "si-base-units";
-	this.temperatureUnits = " C";
-	this.velocityUnits = " m/s";
+	this.temperatureUnits = "C";
+	this.velocityUnits = "m/s";
 	this.temperatureId = doc.getElementById("temperature");
 	this.temperatureUnitsId = doc.getElementById("temperature-units");
+	this.tempMinId = doc.getElementById("temp-min");
+	this.tempMinUnitsId = doc.getElementById("temp-min-units");
+	this.tempMaxId = doc.getElementById("temp-max");
+	this.tempMaxUnitsId = doc.getElementById("temp-max-units");
 	this.locationId = doc.getElementById("location");
 	this.iconId = doc.getElementById("icon");
 	this.descriptionId = doc.getElementById("description");
@@ -63,8 +69,10 @@ function getWeather(loc) {
 			var weatherObject = new Weather();
 			weatherObject.weatherState = data.weather[0].icon;
 			weatherObject.temperature = data.main.temp;
+			weatherObject.tempMin = data.main.temp_min;
+			weatherObject.tempMax = data.main.temp_max;
 			weatherObject.location = data.name;
-			weatherObject.description = data.weather[0].description;
+			weatherObject.description = data.weather[0].main;
 			weatherObject.humidity = data.main.humidity;
 			weatherObject.velocity = data.wind.speed;
 			weatherObject.direction = data.wind.deg;
@@ -76,28 +84,32 @@ function getWeather(loc) {
 function updateScreen(screen, weather) {
 	screen.temperatureId.innerHTML = Math.round(weather.temperature);
 	screen.temperatureUnitsId.innerHTML = screen.temperatureUnits;
+	screen.tempMinId.innerHTML = Math.round(weather.tempMin);
+	screen.tempMinUnitsId.innerHTML = screen.temperatureUnits;
+	screen.tempMaxId.innerHTML = Math.round(weather.tempMax);
+	screen.tempMaxUnitsId.innerHTML = screen.temperatureUnits;
 	screen.locationId.innerHTML = weather.location;
 	screen.iconId.src = iconLink + weather.weatherState + ".png";
 	screen.descriptionId.innerHTML = weather.description;
 	screen.humidityId.innerHTML = weather.humidity;
 	screen.velocityId.innerHTML = Math.round(weather.velocity);
 	screen.velocityUnitsId.innerHTML = screen.velocityUnits;
-	screen.directionId.innerHTML = weather.direction;
+	screen.directionId.innerHTML = toVerbose(weather.direction);
 };
 function switchUnits(screen, units) {
 	if (screen.currentUnits == units) return;
 	if (units == "si-base-units") {
 		screen.currentUnits = "si-base-units";
 		screen.temperatureId.innerHTML = toCelsius(screen.temperatureId.innerHTML);
-		screen.temperatureUnits = " C";
+		screen.temperatureUnits = "C";
 		screen.velocityId.innerHTML = toMs(screen.velocityId.innerHTML);
-		screen.velocityUnits = " m/s";
+		screen.velocityUnits = "m/s";
 	} else {
 		screen.currentUnits = "us-customary-units";
 		screen.temperatureId.innerHTML = toFahrenheit(screen.temperatureId.innerHTML);
-		screen.temperatureUnits = " F";
+		screen.temperatureUnits = "F";
 		screen.velocityId.innerHTML = toMph(screen.velocityId.innerHTML);
-		screen.velocityUnits = " mph";
+		screen.velocityUnits = "mph";
 
 	}
 	// TODO: update screen
@@ -115,4 +127,15 @@ function toMph(value) {
 };
 function toMs(value) {
 	return Math.round(value / 2.25);
+};
+function toVerbose(value) {
+	var slice = 360 / 8;
+	var angle = value % 360;
+	var directions = ["N", "NE", "E", "SE", "S", "SW", "W", "NW"];
+	for (var i = 0; i < directions.length; i++) {
+		if (angle >= slice * (i - 0.5) && angle <= slice * (i + 0.5)) {
+			return directions[i];
+		}
+	}
+	return "indeed?";
 };
